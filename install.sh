96b14af58f1373d9f414ec3cc5e3ff504f146c41
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2023-09-28 17:58:05 +0900

    'install.sh'

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-conda create -n molvae python=3.9
+conda create -n molvae python=3.9 -y
 conda install -c conda-forge rdkit -y
 conda install -c pytorch pytorch torchvision torchaudio cpuonly -y
 conda install -c anaconda scikit-learn -y
