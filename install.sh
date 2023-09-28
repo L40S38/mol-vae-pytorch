6889023250da4aa8c82037d5cc54c1bee0381023
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2023-09-28 17:16:21 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+conda create -n molvae python=3.9
+conda install -c conda-forge rdkit -y
+conda install -c pytorch pytorch torchvision torchaudio cpuonly -y
+conda install -c anaconda scikit-learn -y
+conda install -c conda-forge tdqm -y
+python -m pip install pyyaml
