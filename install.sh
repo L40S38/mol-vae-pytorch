f7ec1ae27bd88c2d3571641fd7db7e0805786f98
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2023-09-29 11:54:47 +0900

    install.sh modified

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ conda create -n molvae python=3.9 -y
 conda activate molvae
 conda install -c conda-forge rdkit -y
 conda install -c pytorch pytorch torchvision torchaudio cpuonly -y
+#pip install torch torchvision torchaudio #GPU環境を利用する場合はこちらを推奨
 conda install -c anaconda scikit-learn -y
 conda install -c conda-forge tdqm -y
 python -m pip install pyyaml
