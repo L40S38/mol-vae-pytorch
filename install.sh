19d28b87cffa93b0d994b732ecb5b5fa49c9e9c7
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2023-09-29 18:40:15 +0900

    install.sh modified

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ conda install -c pytorch pytorch torchvision torchaudio cpuonly -y
 #pip install torch torchvision torchaudio #GPU環境を利用する場合はこちらを推奨
 conda install -c anaconda scikit-learn -y
 conda install -c anaconda seaborn -y
-conda install -c conda-forge tdqm -y
+conda install -c conda-forge tqdm -y
 python -m pip install pyyaml
